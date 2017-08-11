4b4000f274af21fa4651725bc90c6970cce0c52a
Tancredi Orlando <tancredi.orlando@gmail.com>
2017-08-11 14:47:52 +0200

    Refactoring.
    * Remove get_frames function
    * Replace try block with list comprehension
    * Replace continue with if/else

--- bowling.py
+++ bowling.py
@@ -1,56 +1,35 @@
 #!/usr/bin/env python3
 import sys
-from itertools import chain
+import re
 
 
-def get_frames(frames_str):
+def get_throws(frames_str):
     """
-    Return a list of frames given a string.
+    Return a list of throws given a string representing a complete game.
 
     Given scenario.
-    >>> get_frames("| 1 4 | 4 5 | 6 4 | 5 5 | 10 _ "
+    >>> get_throws("| 1 4 | 4 5 | 6 4 | 5 5 | 10 _ "
     ...            "| 0 1 | 7 3 | 6 4 | 10 _ | 2 8 6 |")
-    [['1', '4'], ['4', '5'], ['6', '4'], ['5', '5'], ['10', '_'], ['0', '1'], \
-['7', '3'], ['6', '4'], ['10', '_'], ['2', '8', '6']]
-    """
-    return [frame.lstrip().rstrip().split(' ')
-            for frame in frames_str.split('|')
-            if frame]
-
-
-def get_throws(frames):
-    """
-    Return a list of throws given a list of frames.
-
-    Given scenario.
-    >>> get_throws([['1', '4'], ['4', '5'], ['6', '4'], ['5', '5'], ['10', '_'],
-    ...             ['0', '1'], ['7', '3'], ['6', '4'], ['10', '_'],
-    ...             ['2', '8', '6']])
     [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 0, 1, 7, 3, 6, 4, 10, 0, 2, 8, 6]
 
     Even number of throws.
-    >>> get_throws([['1', '4'], ['4', '5'], ['6', '4'], ['5', '5'], ['10', '_'],
-    ...             ['0', '1'], ['7', '3'], ['6', '4'], ['10', '_'],
-    ...             ['10', '8']])
-    [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 0, 1, 7, 3, 6, 4, 10, 0, 10, 8, 0]
-
+    >>> get_throws("| 1 4 | 4 5 | 6 4 | 5 5 | 10 _ "
+    ...            "| 0 1 | 7 3 | 6 4 | 10 _ | 2 8 |")
+    [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 0, 1, 7, 3, 6, 4, 10, 0, 2, 8, 0]
     """
-    throws = list(chain.from_iterable(frames))
 
-    for i, throw in enumerate(throws):
-        try:
-            throws[i] = int(throw)
+    r = re.compile('\d+|_')
+    throws = r.findall(frames_str)
 
-        # Is it '_'?
-        except ValueError:
-            throws[i] = 0
+    throws_sane = [0 if throw == '_' else int(throw)
+                   for throw in throws]
 
     # If there no third throw in the last frame then just make the list odd
     # adding a trailing zero
-    if not len(throws) % 2:
-        throws.append(0)
+    if not len(throws_sane) % 2:
+        throws_sane.append(0)
 
-    return throws
+    return throws_sane
 
 
 def get_score(throws):
@@ -84,8 +63,6 @@ def get_score(throws):
     total = 0
 
     for i in range(0, len(throws)-2, 2):
-        frame = (i+2)//2
-
         # By default you just sum two throws from the frame
         score = throws[i] + throws[i+1]
 
@@ -96,17 +73,13 @@ def get_score(throws):
             if throws[i+1] == 10:
                 score += throws[i+2]
 
+            frame = (i+2)//2
             if frame != LAST_FRAME:
                 score += throws[i+2] + throws[i+3]
 
-            total += score
-            continue
-
         # Spare
-        if score == 10:
+        elif score == 10:
             score += throws[i+2]
-            total += score
-            continue
 
         total += score
 
@@ -121,8 +94,7 @@ if __name__ == '__main__':
     with open(sys.argv[1]) as f:
         frames_str = f.read().rstrip()
 
-    frames = get_frames(frames_str)
-    throws = get_throws(frames)
+    throws = get_throws(frames_str)
 
     score = get_score(throws)
     print(score)
